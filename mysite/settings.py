9783e0ef68e6b4380ac685165a10dd88b01c3d49
rbaru <rbaruah@mac.com>
2020-02-07 14:33:08 +0400

    reset debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+sf!i$8i=*+y7!a(pj5rp@_g%d#feuap_lx=6kx^#kpx##@v-^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
